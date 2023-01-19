6dd33b2ca867d2c1b45444acab176c41e402e40b
AlexOrange33 <salbinosss@gmail.com>
2023-01-19 15:45:46 +0300

    Homework6 is done

---
+++ Example41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+System.Console.WriteLine("Введите массив чисел через пробел: ");
+string stringArray = Console.ReadLine()!;
+string[] nums = GetStringMass(stringArray);
+
+
+System.Console.WriteLine(String.Join(",", nums) +" -> "+ UpZero(nums));
+
+
+//---------metods-----------
+string[] GetStringMass(string stringArray)
+{
+    string[] nums = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    return nums;
+}
+
+int UpZero(string[] nums)
+{
+    int[] temp = new int[nums.Length];
+    int count = 0;
+    for(int i = 0; i < nums.Length;i++)
+    {
+        temp[i] = int.Parse(nums[i]);
+        if(temp[i] > 0) count++;
+    }
+    return count;
+}
---
+++ Example43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Написать программу, которая на вход принимает массив из любого количества элементов (не менее 6)в промежутке от 0 до 100, 
+// а на выходе выводит этот же массив, но отсортированный по возрастанию(от меньшего числа к большему).
+
+System.Console.WriteLine("Введите размерность массива: ");
+int[] array = GetArray(int.Parse(Console.ReadLine()!));
+System.Console.WriteLine($"[{String.Join("|",array)}]");
+array = SortArray(array);
+System.Console.WriteLine($"[{String.Join("|",array)}]");
+
+
+//-----metods-----
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0;i<size;i++)
+    {
+        array[i] = new Random().Next(0,100);
+    }
+    return array;
+}
+
+int[] SortArray(int[] array)
+{
+    for(int i = 0; i < array.Length-1; i++)
+    {
+        int maxIndexElement = i;
+        for(int j = i+1;j < array.Length;j++)
+        {
+            if(array[j]<array[maxIndexElement]) maxIndexElement = j;
+        }
+        int temp = array[i];
+        array[i] = array[maxIndexElement];
+        array[maxIndexElement] = temp;
+    }
+    return array; 
+}
